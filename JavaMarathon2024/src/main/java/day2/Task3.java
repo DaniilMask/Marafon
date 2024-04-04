e96f910bfa5a267eec99445acabcdace6b0da659
Daniil_Lemsky <danilmask8@gmail.com>
2024-04-05 00:13:02 +0300

    added days 2

--- JavaMarathon2024/src/main/java/day2/Task3.java
+++ JavaMarathon2024/src/main/java/day2/Task3.java
@@ -1,7 +1,21 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите 1 число");
+        int a = in.nextInt();
+        System.out.println("Введите 2 число");
+        int b = in.nextInt();
+        int i = a + 1;
+        while (i < b ){
+            if (i % 5 == 0 && i % 10 != 0) {
+                System.out.println(i+ " ");
+            }
+            i++;
+        }
 
     }
 }
