929ebd619d4ec46fadb76e3e7e9e1efe28e03da0
Daniil_Lemsky <danilmask8@gmail.com>
2024-04-05 12:06:46 +0300

    complete day 3 tasks

--- JavaMarathon2024/src/main/java/day3/Task2.java
+++ JavaMarathon2024/src/main/java/day3/Task2.java
@@ -1,7 +1,21 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+Scanner in = new Scanner(System.in);
+while (true) {
+    System.out.println("Введите первое число");
+    int x = in.nextInt();
+    System.out.println("Введите второе число");
+    int y = in.nextInt();
+    if (y == 0) {
+        break;
+    }
+      int z = x / y;
+        System.out.println(z);
 
+}
     }
 }
